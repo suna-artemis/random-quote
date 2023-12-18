01b906af8402b02e88296a7d685c3660a4d9b48f
suna <suna.artemis@gmail.com>
2023-12-18 21:18:01 +0800

    feat✨: demo01

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useEffect, useState } from 'react';
+import { useRequest } from 'ahooks';
+
+interface QuoteProps {
+  quote: string;
+  author: string;
+  fetchNewQuote: () => void;
+}
+
+const Quote = ({ quote, author, fetchNewQuote }: QuoteProps) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='quote-box'>
+      <div id='text'>{quote}</div>
+      <div id='author'>{author}</div>
+      <div id='new-quote' onClick={fetchNewQuote}>{`fetch new quote`}</div>
+      <a
+        target='_blank'
+        href='https://twitter.com/intent/tweet'
+        id='tweet-quote'
+        rel='noreferrer'>{`tweet current quote`}</a>
     </div>
+  );
+};
+
+function App() {
+  type Quote = { quote: string; author: string };
+  const [quotes, setQuotes] = useState<Quote[]>([]);
+  const [quoteObj, setQuoteObj] = useState<Quote>();
+
+  const fetchNewQuote = async () => {
+    const res = await fetch(
+      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+    );
+    const quotesRes = await res.json();
+    const quotes = quotesRes.quotes;
+    setQuotes(quotes);
+    const index = Math.trunc(Math.random() * quotes.length);
+    setQuoteObj(quotes[index]);
+  };
+
+  const handleFetchNewQuote = () => {
+    if (quotes.length) {
+      const index = Math.trunc(Math.random() * quotes.length);
+      setQuoteObj(quotes[index]);
+    } else {
+      fetchNewQuote();
+    }
+  };
+
+  useEffect(() => {
+    fetchNewQuote();
+  }, []);
+
+  return (
+    <>
+      {quoteObj && (
+        <Quote
+          quote={quoteObj.quote}
+          author={quoteObj.author}
+          fetchNewQuote={handleFetchNewQuote}
+        />
+      )}
+    </>
   );
 }
 
